db93919a17d5c17e1662865f0d9d5cd57821527a
KMI1203 <cami.zegales.tab@gmail.com>
2024-10-09 13:01:43 -0400

    Update ejercicio1_b.py

--- ejercicio1_b.py
+++ ejercicio1_b.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 import serial
-import smtplib # Se utiliza para mandar los mensajes
-from email.message import EmailMessage # Se utiliza para configurar los mensajes de email
-from abc import ABC, abstractmethod  # Importar las clases base abstractas
+import smtplib
+from email.message import EmailMessage
+from abc import ABC, abstractmethod
 
 # Clase abstracta para manejar la fase de espera del vehículo
 class VehicleState(ABC):
@@ -13,38 +13,33 @@ class VehicleState(ABC):
 
 # Clase que implementa la fase de espera
 class StandbyMode(VehicleState):
-    def __init__(self):
+    def _init_(self):
         self.is_standby = True
 
     def standby(self):
         if self.is_standby:
             print("El vehículo está en modo de espera.")
-            # Aquí puedes agregar más lógica si es necesario
-            time.sleep(1)  # Mantener en espera un segundo
+            time.sleep(1)
 
-en = [14, 15, 27, 22]  # El orden para conectar al puente H es EN1, EN4, EN2, EN3
-o1 = 13  # Se conecta al ENA en el puente H
-o2 = 19  # Se conecta al ENB en el puente H
-
+# Pines para motor y sensor
+en = [14, 15, 27, 22]
+o1 = 13
+o2 = 19
 trig = 23
 echo = 24
 
-sense = 0  # Variable para controlar el sentido de giro
-mail = 0  # Variable para controlar el envío del mensaje
-dist = 0  # Variable para almacenar el valor del sensor de distancia
-value = '0'  # Valor con el que se controla el movimiento del auto por UART o SSH
-m = '0'  # Valor para cambiar la recepción del movimiento por SSH o por UART
-s = '0'  # Valor para almacenar el modo en el que se reciben los datos 0: UART, 1: SSH
+sense = 0  # Sentido de giro
+dist = 0  # Distancia del sensor
+mail = 0  # Estado de envío de email
 
-emaildir = ""  # Almacena la dirección del correo electrónico para enviar el mail
-emailpass = ""  # La contraseña tiene que ser la configurada para aplicaciones de terceros del email configurado
+emaildir = ""  # Dirección de email
+emailpass = ""  # Contraseña de email
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-# Mensajes y encabezados para el mensaje email
 messages = ["The car was stopped\nA nearby object was detected", "Car functioning"]
 subjects = ["Stopped Car", "Car initialized"]
 
@@ -61,42 +56,12 @@ out2.start(0)
 
 def movement(val, d):
     global sense
-    global mail
-    if val == '0':
-        if d < 5:
-            mail = 1
+    if val == '0':  # Detener
         out1.ChangeDutyCycle(0)
         out2.ChangeDutyCycle(0)
-    elif val == '1':
-        sense = 0
-        if mail == 1:
-            mail = 2
-        GPIO.output(en[:2], 1)
-        GPIO.output(en[2:], 0)
-        out1.ChangeDutyCycle(50)
-        out2.ChangeDutyCycle(50)
-    elif val == '2':
-        sense = 1
-        GPIO.output(en[:2], 0)
-        GPIO.output(en[2:], 1)
-        out1.ChangeDutyCycle(50)
-        out2.ChangeDutyCycle(50)
-    elif val == '3':
-        if sense == 1:
-            GPIO.output(en, 0)
-            GPIO.output(en[3], 1)
-        else:
-            GPIO.output(en, 0)
-            GPIO.output(en[0], 1)
-        out1.ChangeDutyCycle(50)
-        out2.ChangeDutyCycle(50)
-    elif val == '4':
-        if sense == 1:
-            GPIO.output(en, 0)
-            GPIO.output(en[2], 1)
-        else:
-            GPIO.output(en, 0)
-            GPIO.output(en[1], 1)
+    elif val == '3':  # Girar a la derecha
+        GPIO.output(en, 0)
+        GPIO.output(en[3] if sense == 1 else en[0], 1)
         out1.ChangeDutyCycle(50)
         out2.ChangeDutyCycle(50)
 
@@ -104,79 +69,48 @@ def distance():
     GPIO.output(trig, GPIO.HIGH)
     time.sleep(0.00001)
     GPIO.output(trig, GPIO.LOW)
-    while True:
+    while GPIO.input(echo) == GPIO.LOW:
         pulso_inicio = time.time()
-        if GPIO.input(echo) == GPIO.HIGH:
-            break
-    while True:
+    while GPIO.input(echo) == GPIO.HIGH:
         pulso_fin = time.time()
-        if GPIO.input(echo) == GPIO.LOW:
-            break
     duracion = pulso_fin - pulso_inicio
-    distancia = (34300 * duracion) / 2
-
-    return distancia
+    return (34300 * duracion) / 2
 
 def emailSent(mess, sub):
     global mail
-    global emaildir
-    global emailpass
     if mail != 0:
         msg = EmailMessage()
         msg['From'] = "Raspberry Pi Car"
         msg['To'] = "emmanuel.idiaquez@ucb.edu.bo"
-        if mail == 1:
-            msg['Subject'] = sub[0]
-            msg.set_content(mess[0])
-        elif mail == 2:
-            msg['Subject'] = sub[1]
-            msg.set_content(mess[1])
-
+        msg['Subject'] = sub[mail - 1]
+        msg.set_content(mess[mail - 1])
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
         s.login(emaildir, emailpass)
         s.send_message(msg)
         s.quit()
         mail = 0
-        del msg
 
 # Crear una instancia de StandbyMode
 standby_mode = StandbyMode()
 
 while True:
     try:
-        file = open("config.txt", 'r')
-        config = file.readlines()
-        m = config[0].rstrip()[-1]
-        s = config[1].rstrip()[-1]
         dist = round(distance(), 2)
-        if dist < 5:
-            value = '0'
-            # emailSent(messages, subjects)
-            print("Email sent")
-        else:
-            if m == '0':
-                if ser.in_waiting > 0:
-                    value = ser.readline().decode('utf-8').rstrip()
-            elif m == '1':
-                value = s
-            else:
-                out1.ChangeDutyCycle(0)
-                out2.ChangeDutyCycle(0)
-                print('Incorrect mode value')
 
-        # Nueva lógica para girar a la derecha cuando no se detecta un objeto
-        if dist >= 5:  # Si no se detecta un objeto
-            movement('3', dist)  # Girar a la derecha (valor '3')
-        else:
-            movement(value, dist)
-            
-        # Lógica para la fase de espera
-        if value == '0':
-            standby_mode.standby()  # Entrar en modo de espera si el vehículo está detenido
-            
+        # Si la distancia es menor a 5, el auto se detiene y envía un email
+        if dist < 5:
+            movement('0', dist)
+            mail = 1  # Indica que se debe enviar un email
+            emailSent(messages, subjects)
+            standby_mode.standby()  # Entra en modo de espera
+
+        # Si la distancia es mayor o igual a 5, el auto gira a la derecha
+        elif dist >= 5:
+            movement('3', dist)  # Gira a la derecha
+
         d = str(dist * 100) + '\n'
         ser.write(d.encode('utf-8'))
-        file.close()
+        
     except Exception as e:
-        print(e)
+        print(e)
